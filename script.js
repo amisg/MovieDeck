df6bffbedd7cbb69c67d0102717ff6fe9fec3688
amisg <aamithsg18@gmail.com>
2024-04-03 14:29:47 +0530

    sortBy popularity and pagination basics done

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentPage = 1;
+
 // selectors
 
 const movieListContainer = document.getElementById("movie-list");
@@ -21,12 +23,105 @@ const pageNumberButtonElement = document.getElementById("page-number-button");
 
 // 1.fetch data
 
-async function fetchData() {
-	const url =
-		"https://api.themoviedb.org/3/search/movie?query=Jack+Reacher&api_key=f0bea3723e095ad51220a30980e83c22";
-	const response = await fetch(url);
-	const data = await response.json();
-	return data;
+function remapData(movieList = []) {
+	const modifiedList = movieList.map((movieObj) => {
+		return {
+			id: movieObj.id,
+			title: movieObj.title,
+			posterPath: movieObj.poster_path,
+			popularity: movieObj.popularity,
+			voteAverage: movieObj.vote_average,
+		};
+	});
+	return modifiedList;
 }
 
-console.log(fetchData());
+async function fetchData(pageNumber, sortingOption) {
+	let url = "";
+	if (sortingOption) {
+		url = `https://api.themoviedb.org/3/discover/movie?api_key=f0bea3723e095ad51220a30980e83c22&language=en-US&page=${pageNumber}&sort_by=popularity.desc`;
+	} else {
+		url = `https://api.themoviedb.org/3/movie/top_rated?api_key=f0bea3723e095ad51220a30980e83c22&language=en-US&page=${pageNumber}`;
+	}
+	const response = await fetch(url);
+	const data = await response.json();
+	const results = data["results"];
+	const modifiedList = remapData(results);
+	console.log(modifiedList);
+	return modifiedList;
+}
+
+// render data
+
+function renderMovies(movieList = []) {
+	movieList.forEach((movie) => {
+		const movieCardHtml = createCardForMovie(movie);
+		movieListContainer.append(movieCardHtml);
+	});
+}
+
+function createCardForMovie(movie) {
+	const { id, title, posterPath, popularity, voteAverage } = movie;
+	const imageLink = "https://image.tmdb.org/t/p/original/" + posterPath;
+	const cardContainerElement = document.createElement("div");
+	cardContainerElement.id = id;
+	cardContainerElement.classList.add("card");
+
+	cardContainerElement.innerHTML = `
+	<section>
+		<img class="poster" src=${imageLink} alt="movie image" />
+	</section>
+	<p class="title">${title}</p>
+	<section class="votes-favourites">
+		<section class="votes">
+			<p class="vote-count">Votes: ${voteAverage}</p>
+			<p class="popularity-count">Popularity: ${popularity}
+		</section>
+		<section class="favourites">
+			heart
+		</section>
+	</secton>
+	`;
+	return cardContainerElement;
+}
+
+function clearMovies() {
+	movieListContainer.innerHTML = "";
+}
+
+async function facilitator(sortOption = false) {
+	const data = await fetchData(currentPage, sortOption);
+	clearMovies();
+	renderMovies(data);
+}
+
+facilitator();
+//pagination
+
+// click listeners
+
+prevButtonElement.addEventListener("click", () => {
+	currentPage--;
+	facilitator();
+	pageNumberButtonElement.innerHTML = `currentPage: ${currentPage}`;
+	// if (currentPage === 1) {
+	// 	prevButtonElement.disabled = true;
+	// } else {
+	// 	prevButtonElement.disabled = false;
+	// }
+});
+
+nextButtonElement.addEventListener("click", () => {
+	currentPage++;
+	facilitator();
+	pageNumberButtonElement.innerHTML = `currentPage: ${currentPage}`;
+	// if (currentPage === 1) {
+	// 	nextButtonElement.disabled = false;
+	// } else {
+	// 	nextButtonElement.disabled = true;
+	// }
+});
+
+sortByRateButtonElement.addEventListener("click", () => {
+	facilitator(true);
+});
